047d8932772521fb1bf5d21155e5cb12c9314e98
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-03-13 14:57:47 -0700

    Update login.php

--- login.php
+++ login.php
@@ -13,14 +13,14 @@
 		$result = $stmt->fetch();
 		
 	if(empty($result)) {
-		echo "Error";	
+		echo "error";	
 	}
 	else {
 		//user_id and username sessions for logged in user,
 		//SESSION will be replace with SharedPreferences from Android
 		$_SESSION['username'] = $result['username'];
 		$_SESSION['user_id'] = $result['user_id'];
-		echo "Success";
+		echo "success";
 	}	
 	
 
