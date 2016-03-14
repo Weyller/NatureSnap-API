1db33db0e61ba7d80bb8af16e16e6200be1a9254
Juan <791171@gmail.com>
2016-03-14 12:10:24 -0700

    Reorganized index.php and style.css

--- index.php
+++ index.php
@@ -13,10 +13,77 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<link href="/css/style.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
+	<div class="form-signin">
+<form method="post" action="login.php" enctype="multipart/form-data">
+	<h3>Sign In</h3>
+	Username: </br><input placeholder="jane" type="text" name="username" /></br>
+	Password: </br><input placeholder="doe" type="password" name="password" /></br>
+	<input type="submit" name="loginForm" value="Login">
+</form>
+
+      <p>User: jane</p>
+      <p>Pass: doe</p>
+     </div>
+     <div class="form-signup">
+     	<form method="post" action="register.php" enctype="multipart/form-data">
+
+	<h3>Sign Up</h3>
+	Name: </br><input type="text" name="name" /></br>
+	Last Name: </br><input type="text" name="last_name" /></br>
+	Email: </br><input type="text" name="email" /></br>
+	Username: </br><input type="text" name="username" /></br>
+	Password: </br><input type="password" name="password" /></br>
+	<input type="submit" name="registerForm" value="Register">     	
+     	</form>
+     </div>
+     <div class="search">
+         <h3>Search Photos</h3>
+      <form action="searchPhoto.php" method="GET" id="search">
+      	<input placeholder="nature" type="text" name="search">
+      </form>
+      <button type="submit" form="search" value="Submit">Search</button>
+</div>
+      
+      <?php if(!empty($_SESSION['username'])){?><h3 class="logged">Current logged in user: <?=$_SESSION['username']?></h3><?php }?>
+
+      <div class="form">
+          <h3>Upload Image</h3>
+      <form method="post" enctype="multipart/form-data" action="imageUploader.php">
+          <input type="file" name="filename" />
+          <br>Description: <br><input type="text" name="description" /><br>
+          <input type="submit" value="Upload" name="uploadForm" />
+      </form>
+</div>
+
+      <div class="form">
+          <h3>Update Image</h3>
+      <form method="post" enctype="multipart/form-data" action="updateImage.php">
+          <input type="file" name="filename" />
+          <br>Photo ID: <br><input type="text" name="photo_id" />
+          <br>Description: <br><input type="text" name="description" /><br>
+          <input type="submit" value="Update" name="updateForm" />
+      </form>
+</div>
+
+      <div class="form">
+          <h3>Delete Image</h3>
+      <form method="post" action="deletePhoto.php">
+          Photo ID: <br><input type="text" name="photo_id" />
+          <br><input type="submit" value="Delete" name="deleteForm" />
+      </form>
+</div>
+<div class="items">
+<?=getUserImages()?>
+</div>
+<br/>
+<div class="items">
+<?=geAllImages()?>
+</div>
+<br><br>
 	<table border="1">
   <tr>
     <th>Description</th>
@@ -56,11 +123,11 @@
   </tr>
      <tr class="working">
     <td>JSON Parsing</td>
-    <td>Juan</td>
+    <td></td>
   </tr>
       <tr class="working">
     <td>API Endpoints Documentation</td>
-    <td>Juan</td>
+    <td></td>
   </tr>
 </table>
 <br>
@@ -124,74 +191,6 @@
   </tr>
 </table>
 <br>
-	<div class="form-signin">
-<form method="post" action="login.php" enctype="multipart/form-data">
-	<h3>Sign In</h3>
-	Username: </br><input placeholder="jane" type="text" name="username" /></br>
-	Password: </br><input placeholder="doe" type="password" name="password" /></br>
-	<input type="submit" name="loginForm" value="Login">
-</form>
-
-      <p>User: jane</p>
-      <p>Pass: doe</p>
-     </div>
-     <div class="form-signup">
-     	<form method="post" action="register.php" enctype="multipart/form-data">
-
-	<h3>Sign Up</h3>
-	Name: </br><input type="text" name="name" /></br>
-	Last Name: </br><input type="text" name="last_name" /></br>
-	Email: </br><input type="text" name="email" /></br>
-	Username: </br><input type="text" name="username" /></br>
-	Password: </br><input type="password" name="password" /></br>
-	<input type="submit" name="registerForm" value="Register">     	
-     	</form>
-     </div>
-     <div class="search">
-      <form action="searchPhoto.php" method="GET" id="search">
-      	Search Photos:<br><input placeholder="nature" type="text" name="search">
-      </form>
-      <button type="submit" form="search" value="Submit">Search</button>
-</div>
-      
-      <h3 class="logged"><?php if(!empty($_SESSION['username'])){?>Current logged in user: <?=$_SESSION['username']?><?php }?></h3>
-
-      <div class="form">
-      <form method="post" enctype="multipart/form-data" action="imageUploader.php">
-          <p>Upload a photo: </p>
-          <input type="file" name="filename" />
-          <br>Description: <br><input type="text" name="description" /><br>
-          <input type="submit" value="Upload" name="uploadForm" />
-      </form>
-</div>
-
-<h3>Update Image</h3>
-      <div class="form">
-      <form method="post" enctype="multipart/form-data" action="updateImage.php">
-          <p>Update a photo: </p>
-          <input type="file" name="filename" />
-          <br>Photo ID: <br><input type="text" name="photo_id" />
-          <br>Description: <br><input type="text" name="description" /><br>
-          <input type="submit" value="Update" name="updateForm" />
-      </form>
-</div>
-
-<h3>Delete Image</h3>
-      <div class="form">
-      <form method="post" action="deletePhoto.php">
-          Photo ID: <br><input type="text" name="photo_id" />
-          <br><input type="submit" value="Delete" name="deleteForm" />
-      </form>
-</div>
-<div class="items">
-<?=getUserImages()?>
-</div>
-<br/>
-<div class="items">
-<?=geAllImages()?>
-</div>
-
-
 
 </div>
 
