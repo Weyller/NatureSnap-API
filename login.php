957836119ef06e86e814129a0f2464b33746365c
Juan Vargas <791171@gmail.com>
2016-03-25 15:55:13 -0700

    Update login.php

--- login.php
+++ login.php
@@ -2,25 +2,25 @@
 	session_start();
 	require '../db.php';
 	$dbConn = getConnection();
-    if(!empty($_POST['username']) && !empty(SHA1($_POST['password']))){
-        $sql = "SELECT * FROM users WHERE username=:username AND password=:password";
-        $namedParameters = array();
-        $namedParameters[":username"] = $_POST['username'];
-        $namedParameters[":password"] = SHA1($_POST['password']);
-        $stmt = $dbConn -> prepare($sql);
-        $stmt -> execute($namedParameters);
-        $result = $stmt->fetch();
-        if($result['username'] == $_POST['username'] && $result['password'] == SHA1($_POST['password']) ){
-            updateDate($result['username'], SHA1($_POST['password']));
-            $_SESSION['username'] = $result['username'];
-            $_SESSION['user_id'] = $result['user_id'];
-            echo "success";
-        } else {
-            echo "error";
-        }
-    } else {
-    echo "invalid";	
-}
+    	if(!empty($_POST['username']) && !empty(SHA1($_POST['password']))){
+	        $sql = "SELECT * FROM users WHERE username=:username AND password=:password";
+	        $namedParameters = array();
+	        $namedParameters[":username"] = $_POST['username'];
+	        $namedParameters[":password"] = SHA1($_POST['password']);
+	        $stmt = $dbConn -> prepare($sql);
+	        $stmt -> execute($namedParameters);
+	        $result = $stmt->fetch();
+	        if($result['username'] == $_POST['username'] && $result['password'] == SHA1($_POST['password']) ){
+	            updateDate($result['username'], SHA1($_POST['password']));
+	            $_SESSION['username'] = $result['username'];
+	            $_SESSION['user_id'] = $result['user_id'];
+	            echo "success";
+	        } else {
+	            echo "error";
+	        }
+	    } else {
+	    	echo "invalid";
+	    }
 
 //Coordinated Universal Time (UTC)
 function updateDate($username, $password){
