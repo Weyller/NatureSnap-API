4c6c947688d41b8df18a1d477b15a14431ad9409
Juan <791171@gmail.com>
2016-04-14 09:19:49 -0700

    Separated required and optional parameters conditions

--- imageUploader.php
+++ imageUploader.php
@@ -3,96 +3,100 @@ require '../db.php';
 session_start();
 $dbConn = getConnection();	
 
+//Required parameters
 if(isset($_POST['uploadForm']) 
+   && $_FILES['filename']['tmp_name']
    && !empty($_SESSION['user_id']) 
    && preg_match('/^-?([1-8]?[1-9]|[1-9]0)\.{1}\d{1,6}$/', $_POST['latitude'])
-   && preg_match('/^-?([1]?[1-7][1-9]|[1]?[1-8][0]|[1-9]?[0-9])\.{1}\d{1,6}$/', $_POST['longitude'])
-   || preg_match('/([a-zA-Z0-9_-]+)/s', $_POST['groupName'])
-   || preg_match('/([a-zA-Z0-9_-]+)/s', $_POST['description'])) {
+   && preg_match('/^-?([1]?[1-7][1-9]|[1]?[1-8][0]|[1-9]?[0-9])\.{1}\d{1,6}$/', $_POST['longitude'])) {
     
     //Assign values to variables from inputs
     $user_id = $_SESSION['user_id'];
-    $description = $_POST['description'];
     $latitude = $_POST['latitude'];
     $longitude = $_POST['longitude'];
     
-    //Check if the file input is set
-    if($_FILES['filename']['tmp_name']){
-        $imageType = exif_imagetype($_FILES['filename']['tmp_name'] ); //Returns 1 if gif, 2 if jpg, 3 if png
-        if($imageType !=1 && $imageType !=2 && $imageType !=3) {
-            unlink($_FILES['filename']['tmp_name']);//Delete files
-        } else {
-            //Uploads Directory
-            $target_dir = "uploads";
+    //Optional Parameters
+    if(preg_match('/([a-zA-Z0-9_-]+)/s', $_POST['groupName'])){
+       $group = $_POST['groupName'];
+    } elseif(preg_match('/([a-zA-Z0-9_-]+)/s', $_POST['description'])) {
+       $description = $_POST['description'];
+    }
 
-            //Create directory for each user
-            if (!file_exists($target_dir."/".$_SESSION['user_id'])) { 
-                mkdir($target_dir."/".$_SESSION['user_id'], 0777, true); 
-             }
-            
-            //Set 777 permisions to each user folder to allow deleting and updating folder and files
-            $old = umask(0);
-            chmod($target_dir."/".$_SESSION['user_id'], 0777);
-            umask($old);
+    $imageType = exif_imagetype($_FILES['filename']['tmp_name'] ); //Returns 1 if gif, 2 if jpg, 3 if png
+    if($imageType !=1 && $imageType !=2 && $imageType !=3) {
+        unlink($_FILES['filename']['tmp_name']);//Delete files
+    } else {
+        //Uploads Directory
+        $target_dir = "uploads";
 
-            //Handle empty spaces
-            $photo = str_replace(' ', '', $_FILES['filename']['name']);
-            
-            //Default value for non-existing graoup
-            $group_id = false;
-            
-            //If groupName parameter was provided,
-            //then validate that group with the given parameters
-           if(!empty($_POST['groupName'])){
-                $group = $_POST['groupName'];
-                $group_id = getGroupId($group, $user_id);
-           }
-                               
-            //Check if file exist in user folder or in user group folder
-            if(!empty($_POST['groupName']) && $group_id != false && file_exists($target_dir."/".$_SESSION['user_id']."/".$group_id."/".basename($photo ))){
-                echo "exists";
-            } elseif(empty($_POST['groupName']) && file_exists($target_dir."/".$_SESSION['user_id']."/".basename($photo ))){
-                echo "exists";
-            }   
-            //If file does not exists, then move file into the user folder or group folder
-            else {                
-                if($group_id !=false){
-                    //Initialize group and file paths
-                    $filename = $target_dir."/".$_SESSION['user_id']."/".$group_id."/".$photo;  
-                    $group_dir = $target_dir."/".$_SESSION['user_id']."/".$group_id;
+        //Create directory for each user
+        if (!file_exists($target_dir."/".$_SESSION['user_id'])) { 
+            mkdir($target_dir."/".$_SESSION['user_id'], 0777, true); 
+         }
 
-                    //Check if group exists, also check if photo in group exists
-                    $checkPhotoGroup = photoGroupExist($user_id, $photo, $group);
-                    $checkGroup = groupExist($user_id, $group);
-                    if($checkPhotoGroup != true && $checkGroup != false && file_exists($group_dir)){
-                        move_uploaded_file($_FILES['filename']['tmp_name'], $filename );
-                        addGroupPhoto($user_id, $filename, $description, $checkGroup, $latitude, $longitude);
-                    } else {
-                        echo "error";
-                    }
+        //Set 777 permisions to each user folder to allow deleting and updating folder and files
+        $old = umask(0);
+        chmod($target_dir."/".$_SESSION['user_id'], 0777);
+        umask($old);
+
+        //Handle empty spaces
+        $photo = str_replace(' ', '', $_FILES['filename']['name']);
+
+        //Default value for non-existing graoup
+        $group_id = false;
+
+        //If groupName parameter was provided,
+        //then validate that group with the given parameters
+       if(!empty($_POST['groupName'])){
+            $group_id = getGroupId($group, $user_id);
+       }
+
+        //Check if file exist in user folder or in user group folder
+        if(!empty($_POST['groupName']) && $group_id != false && file_exists($target_dir."/".$_SESSION['user_id']."/".$group_id."/".basename($photo ))){
+            echo "exists";
+        } elseif(empty($_POST['groupName']) && file_exists($target_dir."/".$_SESSION['user_id']."/".basename($photo ))){
+            echo "exists";
+        }   
+        //If file does not exists, then move file into the user folder or group folder
+        else {                
+            if($group_id !=false){
+                //Initialize group and file paths
+                $filename = $target_dir."/".$_SESSION['user_id']."/".$group_id."/".$photo;  
+                $group_dir = $target_dir."/".$_SESSION['user_id']."/".$group_id;
+
+                //Check if group exists, also check if photo in group exists
+                $checkPhotoGroup = photoGroupExist($user_id, $photo, $group);
+                $checkGroup = groupExist($user_id, $group);
+                if($checkPhotoGroup != true && $checkGroup != false && file_exists($group_dir)){
+                    move_uploaded_file($_FILES['filename']['tmp_name'], $filename );
+                    addGroupPhoto($user_id, $filename, $description, $checkGroup, $latitude, $longitude);
+                } else {
+                    echo "error";
                 }
-                
-                //If no group is included, then check for regular photo only
-                if($group_id ==false){
-                    $filename = $target_dir."/". $_SESSION['user_id'] . "/" .basename($photo);
-                    $checkPhoto = photoExist($user_id, $photo);
-                    if($checkPhoto == false){
-                        //Make photos public or private
-                        $private = 0;
-                        if(isset($_POST['private'])){
-                           $private = 1; 
-                        }
-                        addPhoto($user_id, $filename, $description, $private, $latitude, $longitude);
-                    } else {
-                        echo "error";
+            }
+
+            //If no group is included, then check for regular photo only
+            if($group_id ==false){
+                $filename = $target_dir."/". $_SESSION['user_id'] . "/" .basename($photo);
+                $checkPhoto = photoExist($user_id, $photo);
+                if($checkPhoto == false){
+                    //Make photos public or private
+                    $private = 0;
+                    if(isset($_POST['private'])){
+                       $private = 1; 
                     }
+                    addPhoto($user_id, $filename, $description, $private, $latitude, $longitude);
+                } else {
+                    echo "error";
                 }
             }
         }
-    } else {
-        echo "nothing";
     }
-} else {
+
+} elseif(!$_FILES['filename']['tmp_name']){
+    echo "nothing";
+}
+    else {
     echo "invalid";
 }
 //Check if entry for photo  already exists
