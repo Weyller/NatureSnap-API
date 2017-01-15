02e23874ce371b0af4f3f3128b889ce7bff71f08
Juan <791171@gmail.com>
2017-01-15 10:44:59 -0800

    Include Font-Awesome Icons

--- head.php
+++ head.php
@@ -10,6 +10,7 @@
 <!-- CSS Files -->
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="bootstrap/css/album.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet">
 <link href="css/custom.css" rel="stylesheet">
 </head>
 <body>
